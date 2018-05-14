Correct angle calculation between points

crater_detection/detector/__init__.py
@@ -3,9 +3,8 @@ import cv2 as cv
 from typing import Tuple, List, Any
 from scipy.spatial import distance
 from scipy.signal import argrelmax
-from matplotlib import pyplot as plt
 
-from ..util import logger
+from ..util import logger, angle_between_points
 from crater_detection.models import Crater, CraterField
 
 OUTLINE_COLOR = (0, 255, 0)
@@ -82,11 +81,9 @@ def detect(input_image: np.ndarray) -> Tuple[np.ndarray, CraterField]:
     else:
         bw_img = cv.cvtColor(input_image, cv.COLOR_BGR2GRAY)
 
-    craters = []
-
-    logger.info("Finding circles")
-    circles = [] # find_circles(bw_img)
-    logger.info("Found %i total circles" % len(circles))
+    # logger.info("Finding circles")
+    # circles = [] # find_circles(bw_img)
+    # logger.info("Found %i total circles" % len(circles))
 
     min_val, max_val = get_peak_values(bw_img)
     logger.debug("Lowest img value:", np.min(bw_img))
@@ -117,10 +114,6 @@ def detect(input_image: np.ndarray) -> Tuple[np.ndarray, CraterField]:
     # closed = close_image(thresh_image)
     # clean_image(thresh_image)
 
-    # Create crater tree
-    logger.info("Creating Crater list")
-    # craters = list(map(lambda cont: Crater(cont), contours))
-
     # Pair high and low contours
     # for each high contour, find the closest low contour by center
     def mapper(c):
@@ -149,9 +142,8 @@ def detect(input_image: np.ndarray) -> Tuple[np.ndarray, CraterField]:
     dists = np.zeros(shape=(len(high_contours), len(low_contours)))
     h_matches = np.ndarray(shape=(len(high_contours),), dtype=np.uint)
     l_matches = np.ndarray(shape=(len(low_contours),), dtype=np.uint)
-    combinded = []
     craters = []
-    logger.debug("Matching")
+    logger.debug("Matching high and low crater pairs")
     for h_i, h in enumerate(high_contours):
         for l_i, l in enumerate(low_contours):
             dists[h_i][l_i] = distance_between_contours(h, l)
@@ -161,7 +153,6 @@ def detect(input_image: np.ndarray) -> Tuple[np.ndarray, CraterField]:
         h_matches[h_i] = int(min_ind)
         l_matches[min_ind] = h_i
         full_contour = np.append(high_contours[h_i], low_contours[min_ind], axis=0)
-        combinded.append(full_contour)
         craters.append(Crater(high_contours[h_i], low_contours[min_ind], full_contour))
 
 
@@ -175,9 +166,9 @@ def detect(input_image: np.ndarray) -> Tuple[np.ndarray, CraterField]:
     # cv.drawContours(color_image, combinded, -1, (0, 255, 0), 2)
     cv.drawContours(color_image, list(map(lambda c: c.full_contour, craters)), -1, (0, 255, 0), 2)
 
-    logger.info("Drawing circles")
-    for circle in circles:
-        cv.circle(color_image, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
+    # logger.info("Drawing circles")
+    # for circle in circles:
+    #     cv.circle(color_image, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
 
     logger.info("Drawing contour connections")
     for h_i, l_i in enumerate(h_matches):

crater_detection/models/Crater.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 import cv2 as cv
 import numpy as np
-from ..util import angle_between
+from ..util import angle_between_points, angle_between_with_origin
 
 
 class Crater:
@@ -13,9 +13,15 @@ class Crater:
         self.full_contour = combinded_c
 
     def sun_angle(self) -> np.real:
-        low_pos, low_rad = cv.minEnclosingCircle(self.low_contour)
         high_pos, high_rad = cv.minEnclosingCircle(self.high_contour)
-        return angle_between(low_pos, high_pos)
+        low_pos, low_rad = cv.minEnclosingCircle(self.low_contour)
+
+        low_pos = np.uint(np.around(low_pos))
+        high_pos = np.uint(np.around(high_pos))
+
+        btw_circles = angle_between_points(high_pos, low_pos)
+
+        return btw_circles
 
     def radius(self) -> np.real:
         pos, rad = self.min_enclosing_circle()

crater_detection/util.py
@@ -7,19 +7,34 @@ def unit_vector(vector):
     return vector / np.linalg.norm(vector)
 
 
-def angle_between(v1, v2):
+def angle_between_points(p1, p2):
+    delta_x = np.longlong(p1[0]) - np.longlong(p2[0])
+    delta_y = np.longlong(p1[1]) - np.longlong(p2[1])
+    theta_radians = np.arctan2(delta_y, delta_x)
+    return theta_radians
+
+
+def angle_between_vectors(v1, v2):
     """ Returns the angle in radians between vectors 'v1' and 'v2'::
 
-            >>> angle_between((1, 0, 0), (0, 1, 0))
+            >>> angle_between_vectors((1, 0, 0), (0, 1, 0))
             1.5707963267948966
-            >>> angle_between((1, 0, 0), (1, 0, 0))
+            >>> angle_between_vectors((1, 0, 0), (1, 0, 0))
             0.0
-            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            >>> angle_between_vectors((1, 0, 0), (-1, 0, 0))
             3.141592653589793
     """
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def angle_between_with_origin(p0, p1, p2):
+    v0 = np.array(p0) - np.array(p1)
+    v1 = np.array(p2) - np.array(p1)
+
+    angle = np.math.atan2(np.linalg.det([v0, v1]), np.dot(v0, v1))
+    return angle
 
 
 class Logger: